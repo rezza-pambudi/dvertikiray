23a151440e65607eb34a3f2bdc7cfa5d34887564
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-06-11 18:17:23 +0700

    update dvertikiray

--- dvertikiray/src/pages/Home.jsx
+++ dvertikiray/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import Navbar from "../components/Navbar"
 import HomeContent from "../components/HomeContent";
+import Course from "../components/Course";
+import Tutors from "../components/Tutors";
+import Partners from "../components/Partners";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import '../styles/Home.css';
 
@@ -8,10 +12,14 @@ function Home() {
     <>
      <Navbar />
      <HomeContent />
-     <Footer />
      <div className="wrapper">
-
+        <Course />
+        <Tutors />
+        <Partners />
      </div>
+     <Contact />
+     <Footer />
+     
     </>
   )
 }
