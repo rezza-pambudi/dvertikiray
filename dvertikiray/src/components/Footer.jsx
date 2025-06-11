935e3ac079376e12a9bbff85102aa9f725734d43
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-06-11 17:57:17 +0700

    update dvertikiray

---
+++ dvertikiray/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import '../styles/Footer.css';
+function Footer() {
+  return (
+    <>
+    <div id="copyright">
+        <div className="wrapper">
+            &copy; 2020. <b>RumahRafif.</b> All Rights Reserved.
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ dvertikiray/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar"
+import HomeContent from "../components/HomeContent";
+import Footer from "../components/Footer";
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <>
+     <Navbar />
+     <HomeContent />
+     <Footer />
+     <div className="wrapper">
+
+     </div>
+    </>
+  )
+}
+
+export default Home
