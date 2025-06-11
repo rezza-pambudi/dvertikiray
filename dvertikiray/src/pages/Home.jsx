6108b886d642b7eae17ccbbd80e5babcb152bc6c
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-06-11 18:54:15 +0700

    update dverty

--- dvertikiray/src/pages/Home.jsx
+++ dvertikiray/src/pages/Home.jsx
@@ -6,13 +6,21 @@ import Partners from "../components/Partners";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import '../styles/Home.css';
+import { homeSection } from "../data/HomeSection";
+import parse from 'html-react-parser';
 
 function Home() {
   return (
     <>
      <Navbar />
-     <HomeContent />
+     
      <div className="wrapper">
+        <section id="home">
+            <img src={homeSection.image}/>
+            <div className="kolom">
+                {parse(homeSection.content)}
+            </div>
+        </section>
         <Course />
         <Tutors />
         <Partners />
