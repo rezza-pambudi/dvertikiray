82672d5e4d150d5604a070f109349bc6442e8418
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-06-12 12:50:03 +0700

    dverty

--- dvertikiray/src/components/Course.jsx
+++ dvertikiray/src/components/Course.jsx
@@ -1,16 +1,14 @@
+import { courseSection } from '../data/CourseSection';
 import '../styles/Course.css';
+import parse from 'html-react-parser';
 function Course() {
   return (
     <>
     <section id="courses">
             <div className="kolom">
-                <p className="deskripsi">You Will Need This</p>
-                <h2>Online Courses</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
-                <p><a href="" className="tbl-biru">Pelajari Lebih Lanjut</a></p>
+                {parse(courseSection.content)}
             </div>
-            <img src="https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161"/>
+            <img src={courseSection.image}/>
         </section>
     </>
   )
